03f67d363c78fd66043f8210783e1a087ff1b403
HannahFreitas <hannahcassia@gmail.com>
2021-12-08 15:58:10 -0300

    Adicionando uma mensagem no inicio

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const routes = express.Router();
 
-
+routes.get("/",  (req, res) => {
+    res.status(200).send({
+        title: "Reprograma -> Cestas Solidárias - Projeto Final",
+        version: "1.0.0",
+        mensagem: "Olá, ser humano do outro lado da tela, aqui você encontra a primeira versão deste projeto, uma API focada em cadastro de pessoas em situação de vulnerabilidade social."
+    })
+})
 
 module.exports = routes
