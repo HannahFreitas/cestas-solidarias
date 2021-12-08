4c2146382cff929fcfc424c7cdc2728b92a7dbba
HannahFreitas <hannahcassia@gmail.com>
2021-12-08 16:22:39 -0300

    Schema inicial pronto, até então, sem quebra no código

--- src/models/cestasSolidariasSchema.js
+++ src/models/cestasSolidariasSchema.js
@@ -4,6 +4,34 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    }, 
+    email: {
+        type: String,
+        required: true
+    },
+    celular: {
+        type: Number,
+        required: true
+    },
+    whatsapp: {
+        type: Boolean,
+        required: true
+    },
+    cpf: {
+        type: Number,
+        required: true,
+        select: false
+    },
+    bairro: {
+        type: String,
+        required: true
+    }, 
+    pix: {
+        type: String,
+        required: true
     }
 })
 
+const Users = mongoose.model("users", userSchema);
+
+module.exports = Users
