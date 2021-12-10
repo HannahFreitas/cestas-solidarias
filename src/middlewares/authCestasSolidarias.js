67020edbb46f0656006dd2e4466cda638cf60e12
HannahFreitas <hannahcassia@gmail.com>
2021-12-10 20:04:47 -0300

    Ajeitando o middlewares

--- src/middlewares/authCestasSolidarias.js
+++ src/middlewares/authCestasSolidarias.js
@@ -8,14 +8,9 @@ exports.checkAuth = (req, res, next) => {
 
     try {
         const secret = process.env.SECRET;
-        //com ajuda de Rafa
-        jwt.verify(token, secret, (err, decoded) => {
-            if(err) {
-              return res.status(400).json({message: "Token inválido"})
-            }
-            
-            req.userId = decoded.id;
-          });
+
+        jwt.verify(token, secret);
+
         next();
     } catch (error) {
         res.status(400).json({ message: "O Token é inválido!" });
