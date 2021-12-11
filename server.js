3571e38f782fed8840e288ee6fc7942cf80bfb31
HannahFreitas <hannahcassia@gmail.com>
2021-12-11 14:26:50 -0300

    Adicionando uma nova porta local no server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./src/app");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Vemos aqui claramente, que estamos na porta ${PORT}`));
